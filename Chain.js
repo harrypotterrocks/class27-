4f0a658b3ed049fb0f8e1a82da2e27f25defac29
harrypotterrocks <67183983+harrypotterrocks@users.noreply.github.com>
2020-07-26 18:02:04 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Chain {
+    constructor(object1,object2){
+        var options={
+            bodyA:object1,
+            bodyB:object2,
+            stiffness:0.04,
+            length:10
+        }
+    this.chain = Matter.Constraint.create(options);
+        World.add(world,this.chain);
+      }
+    display(){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.chain.bodyB.position;
+            push();
+            strokeWeight(10); 
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            pop();
+    }
+}
